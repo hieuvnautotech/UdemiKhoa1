be6cc465d9461ef80a8e831a6d39f19582883f79
hieu <hieuvnautotech@gmail.com>
2022-12-14 14:40:08 +0700

    bài 4 quản lý state toàn cục

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,18 @@
 import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem";
-
+import FeedbackList from "./components/FeedbackList";
+import { React, useState } from 'react'
+import FeedbackData from "./data/FeedbackData";
 // truyền cái props text wa header, nó có hỗ trợ propsDefault nếu ko có text
 // và có hỗ trợ thư viện loại props truyền qua là string hay boolean
 function App() {
+
+    const [feedback, setFeedback] = useState(FeedbackData);
+
     return (
-        <>
-            <Header />
+      <>
+        <Header />
         <div className="container">
-          <FeedbackItem/>
+          <FeedbackList feedback={feedback} />
         </div>
       </>
     ); 
--- my-app/src/components/FeedbackItem.js
+++ my-app/src/components/FeedbackItem.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from 'react'
 
 // bài 3 là chỉ cách xài useState
 
-function FeedbackItem() {
+function FeedbackItem({ item}) {
     const [rating, setRating] = useState(7)
     const [text, setText] = useState('this is is this')
 
@@ -16,14 +16,14 @@ function FeedbackItem() {
     }
 
   return (
-      <div className="card">
-          <div className="num-display">{rating}</div>
-          <div className="text-display">
-              {text}
-          </div>
-          <button type="button" onClick={handleClick}>Click</button>
+    <div className="card">
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+      <button type="button" onClick={handleClick}>
+        Click
+      </button>
     </div>
-  )
+  );
 }
 
 export default FeedbackItem
