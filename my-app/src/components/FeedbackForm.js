39e6375946e5e73ff13a06f8f6ca82ea09365e31
hieu <hieuvnautotech@gmail.com>
2022-12-16 22:30:26 +0700

    ok

--- my-app/src/components/FeedbackForm.js
+++ my-app/src/components/FeedbackForm.js
@@ -4,16 +4,29 @@ import Button from "./Shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisable, setBtnDisable] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    if(text === ''){
+      setBtnDisable(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10){
+      setMessage('Text must be at least 10 characters')
+      setBtnDisable(true)
+    } else {
+      setMessage(null)
+      setBtnDisable(false)
+    }
+    setText(e.target.value)
   };
 
   return (
     <>
       <Card>
         <form>
-          <h2>ksdjflksdf</h2>
+          <h2>How would you rate your service with us</h2>
           <div className="input-group">
             <input
               type="text"
@@ -21,10 +34,14 @@ function FeedbackForm() {
               onChange={handleTextChange}
               value={text}
             />
-            <Button type='submit'>
+            <Button type='submit'
+              isDisabled={btnDisable}
+            >
                 Send
             </Button>
           </div>
+
+          {message && <div className="message">{message}</div>}
         </form>
       </Card>
     </>
