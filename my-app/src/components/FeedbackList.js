09cff41ca4997893c5abe74e53e8788a3b0b7849
hieu <hieuvnautotech@gmail.com>
2022-12-20 22:43:32 +0700

    xong

--- my-app/src/components/FeedbackList.js
+++ my-app/src/components/FeedbackList.js
@@ -1,33 +1,59 @@
-import React from 'react'
-import FeedbackItem from './FeedbackItem';
+import React from "react";
+import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
-
+import { motion, AnimatePresence } from "framer-motion";
 // bài này chỉ cách xài hàm .map
-
+// bài này chỉ cài npm i framer-motion@4.1.17
+// rồi import vào đây
 function FeedbackList({ feedback, handleDelete }) {
-    if (!feedback || feedback.length === 0) { 
-        return <p>no feedback yet</p>
-    }
+  if (!feedback || feedback.length === 0) {
+    return <p>no feedback yet</p>;
+  }
 
-    return (
-      <div className="feedback-list">
+  return (
+    <div className="feedback-list">
+      <AnimatePresence>
         {feedback.map((item) => (
-        //   <div>{item.rating}</div>
-            <FeedbackItem key={item.id} item={ item} 
-            handleDelete = {handleDelete}/>
-            // handleDelete={(id)=>console.log(id)} 
-            
+          <motion.div
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity:0}}
+          >
+            {/* <div>{item.rating}</div> */}
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+            {/* handleDelete={(id) => console.log(id)} */}
+          </motion.div>
         ))}
-      </div>
-    );
+      </AnimatePresence>
+      ;
+    </div>
+  );
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //     //   <div>{item.rating}</div>
+  //         <FeedbackItem key={item.id} item={ item}
+  //         handleDelete = {handleDelete}/>
+  //         // handleDelete={(id)=>console.log(id)}
+
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired
-  }))
-}
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
-export default FeedbackList
+export default FeedbackList;
