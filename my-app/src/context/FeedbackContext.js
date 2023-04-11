d2d832b6b9f062afe6170276ea3557d9c90e05fe
hieu <hieuvnautotech@gmail.com>
2023-04-11 22:26:07 +0700

    ok

--- my-app/src/context/FeedbackContext.js
+++ my-app/src/context/FeedbackContext.js
@@ -1,14 +1,18 @@
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
 
 const FeedbackContext = createContext()
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id:1,
-            text:'this item is from context',
-            rating:10
-        }
-    ])
+    const [feedback, setFeedback] = useState([])
+    useEffect(()=>{
+        // console.log(123)
+        fetchFeedback()
+    },[])
+    const fetchFeedback = async () =>{
+        const response = await fetch(`http://localhost:5000/feedback?
+        _sort=id&_order=desc`)
+        const data = await response.json()
+        console.log(data)
+    }
   return (
     <FeedbackContext.Provider value={{
         feedback: feedback,
