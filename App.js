54a70da09e73c6e979c19bce38bbb918bf1f45cf
hieu <hieuvnautotech@gmail.com>
2023-04-11 21:12:17 +0700

    làm server gia lap

---
+++ App.js
@@ -0,0 +1,33 @@
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import { React, useState } from "react";
+import FeedbackData from "./data/FeedbackData";
+import Card from "./components/Shared/Card";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+// truyền cái props text wa header, nó có hỗ trợ propsDefault nếu ko có text
+// và có hỗ trợ thư viện loại props truyền qua là string hay boolean
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    // console.log('App', id)
+    if (window.confirm("Are you sure you want to delete")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        {/* <Card>haha</Card> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
