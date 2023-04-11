dabd138314add70fd28854ca9dc0a1dc21d9c35f
hieu <hieuvnautotech@gmail.com>
2023-04-11 22:39:00 +0700

    xong

--- my-app/src/context/FeedbackContext.js
+++ my-app/src/context/FeedbackContext.js
@@ -1,8 +1,35 @@
 import {createContext, useState, useEffect} from 'react'
-
+import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext()
 export const FeedbackProvider = ({children}) => {
+    const [isLoading, setIsloading] = useState(true)
     const [feedback, setFeedback] = useState([])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false,
+      });
+      const deleteFeedback = (id) => {
+        // console.log('App', id)
+        if (window.confirm("Are you sure you want to delete")) {
+          setFeedback(feedback.filter((item) => item.id !== id));
+        }
+      };
+      const updateFeedback = (id, updItem) => { 
+        // console.log(id, updItem)
+        setFeedback(feedback.map((item) => item.id === id ? {
+          ...item, ...updItem } : item))
+      }
+      const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        // console.log(newFeedback)
+        setFeedback([newFeedback, ...feedback]);
+      };
+      const editFeedback = (item) => {
+        setFeedbackEdit({
+          item,
+          edit: true,
+        });
+      };
     useEffect(()=>{
         // console.log(123)
         fetchFeedback()
@@ -12,10 +39,18 @@ export const FeedbackProvider = ({children}) => {
         _sort=id&_order=desc`)
         const data = await response.json()
         console.log(data)
+        setFeedback(data)
+        setIsloading(false)
     }
   return (
     <FeedbackContext.Provider value={{
-        feedback: feedback,
+        feedback,
+        feedbackEdit,
+        isLoading,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
     }}>
         {children}
     </FeedbackContext.Provider>
