c62fa71c626def949942ff6324a7121bc7a3bc5c
hieu <hieuvnautotech@gmail.com>
2023-04-15 09:53:13 +0700

    làm navbar

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-  return (
-    <div>
-      haha
-    </div>
-  );
+  return
+  <Router>
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <main>Content</main>
+    </div>;
+  </Router>;
+  
 }
 
 export default App;
