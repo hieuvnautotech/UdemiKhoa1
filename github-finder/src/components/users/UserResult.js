0134f4d2e21c73cd0c7743347d35d6cefd88b96f
hieu <hieuvnautotech@gmail.com>
2023-04-20 22:37:38 +0700

    chưa xong

---
+++ github-finder/src/components/users/UserResult.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useEffect} from 'react'
+
+function UserResult() {
+    useEffect(() =>{
+        fectchUsers()
+    },[])
+    const fectchUsers = async () => {
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
+            headers:{
+                Authorization:`token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+  return (
+    <div>UserResult</div>
+  )
+}
+
+export default UserResult
--- github-finder/src/pages/Home.js
+++ github-finder/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import UserResult from '../components/users/UserResult'
 
 function Home() {
   return (
-    <div>
-        <h1 className='text-6xl'>Welcome</h1>
-        {process.env.REACT_APP_GITHUB_TOKEN}
-    </div>
+    <>
+      <UserResult/>
+    </>
   )
 }
 
