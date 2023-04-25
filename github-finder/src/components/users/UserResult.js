4b79753406c288a56aa1f642b68d78b5b3a95dfe
hieu <hieuvnautotech@gmail.com>
2023-04-25 22:17:59 +0700

    ok

--- github-finder/src/components/users/UserResult.js
+++ github-finder/src/components/users/UserResult.js
@@ -21,8 +21,12 @@ function UserResult() {
     if(!loading){
         return (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-                {users.map((user)=>(
-                    <h3>{user.login}</h3>
+                {users.map(user=>(
+                        
+                            
+                            <h3>{user.login}</h3>
+                            
+                        
                 ))}
             </div>
           )
