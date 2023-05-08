75e64801dae69e05a486ff35d693e5c712eb08cb
hieu <hieuvnautotech@gmail.com>
2023-05-08 22:47:05 +0700

    ok

---
+++ house-marketplace/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "udemy-46abf.firebaseapp.com",
+  projectId: "udemy-46abf",
+  storageBucket: "udemy-46abf.appspot.com",
+  messagingSenderId: "792059080141",
+  appId: "1:792059080141:web:dcefa042c52358ddbda884"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
---
+++ house-marketplace/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { getDatabase, ref, child, get } from "firebase/database";
+import {database} from './firebase'
+
+
+
+function App() {
+  const dbRef = ref(database);
+  get(child(dbRef, `users`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  return (
+    <div>
+      asdasd
+    </div>
+  );
+}
+
+export default App;
