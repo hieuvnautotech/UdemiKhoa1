1dfc02e678c43e8af1bfea381fb07037d30b8d75
hieu <hieuvnautotech@gmail.com>
2023-05-08 13:36:14 +0700

    ok

--- github-finder/src/context/github/GithubReducer.js
+++ github-finder/src/context/github/GithubReducer.js
@@ -6,12 +6,6 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
-    case "GET_REPOS":
-      return {
-        ...state,
-        repos: action.payload,
-        loading: false,
-      };
     case "SET_LOADING":
       return {
         ...state,
@@ -22,10 +16,11 @@ const githubReducer = (state, action) => {
         ...state,
         users: [],
       };
-    case "GET_USER":
+    case "GET_USER_AND_REPOS":
       return {
         ...state,
-        user: action.payload,
+        user: action.payload.user,
+        repos: action.payload.repos,
         loading: false,
       };
     default:
