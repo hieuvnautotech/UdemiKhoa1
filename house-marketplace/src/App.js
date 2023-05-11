9864291e6da5425c44ef3acd7a28a44e0aa7a35a
hieu <hieuvnautotech@gmail.com>
2023-05-11 22:37:48 +0700

    ok

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,10 +1,26 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Explore from './pages/Explore'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   
   return (
     <>
-      <h1>My app</h1>
+      <Router>
+          <Routes>
+              <Route path='/' element={<Explore/>}/>
+              <Route path='/offers' element={<Offers/>}/>
+              <Route path='/profile' element={<Profile/>}/>
+              <Route path='/sign-in' element={<SignIn/>}/>
+              <Route path='/sign-up' element={<SignUp/>}/>
+              <Route path='/forgot-password' element={<ForgotPassword/>}/>
+          </Routes>
+      </Router>
+      {/* {Navbar} */}
     </>
   );
 }
