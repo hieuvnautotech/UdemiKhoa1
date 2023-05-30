d9f33db5ecb204402872cf57fe1cdccb642a2f34
hieu <hieuvnautotech@gmail.com>
2023-05-30 22:30:25 +0700

    ok

--- house-marketplace/src/pages/Profile.js
+++ house-marketplace/src/pages/Profile.js
@@ -1,14 +1,31 @@
 import {useEffect, useState} from 'react'
 import {getAuth} from 'firebase/auth'
+import { useNavigate, Link } from 'react-router-dom'
 
 function Profile() {
-  const [user, setUser] = useState(null)
   const auth = getAuth()
-  useEffect(() => {
-    console.log(auth.currentUser)
-    setUser(auth.currentUser)
-  },[]) 
-  return user ? <h1>{user.displayName}</h1> : 'Not Logged in yet'
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
+  
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  return <div className='profile'>
+    <header className='profileHeader'>
+      <p className='pageHeader'>My Profile</p>
+      <button type='button' className='logOut' onClick={onLogout}>
+          Logout
+      </button>
+
+    </header>
+  
+  </div>
 }
 
 export default Profile
